f02f79447465bf383d3d292be37f510fb64a0dfb
EnzoMorel <156925785+EnzoMorel@users.noreply.github.com>
2024-01-17 15:37:55 +0100

    Add files via upload

---
+++ include/raise.h
@@ -0,0 +1,20 @@
+/*
+** Enzo Morel, 2024
+** MODULAR C
+** File:
+** raise.h
+*/
+
+#ifndef RAISE_H_
+#define RAISE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define raise(msg)                                              \
+do {                                                            \
+    fprintf(stderr, "%s: %u: %s\n", __FILE__, __LINE__, msg);   \
+    abort();                                                    \
+} while (0)
+
+#endif // RAISE_H_
---
+++ include/vertex.h
@@ -0,0 +1,16 @@
+/*
+** Enzo Morel, 2024
+** MODULAR C
+** File:
+** vertex.h
+*/
+
+#ifndef VERTEX_H_
+#define VERTEX_H_
+
+#include "object.h"
+
+extern const Class *Vertex;
+
+#endif // VERTEX_H_
+
